77f09c4fa06f638b9901fffd7bd06602ea5862b6
James Nock <james@james-nock.co.uk>
2021-11-21 13:04:08 +0000

    Use correct URL for ajax fetch

--- components/SeederGenerator.vue
+++ components/SeederGenerator.vue
@@ -69,7 +69,7 @@ export default {
       formData.append('statements', this.statements)
       await this.$axios({
         method: 'post',
-        url: 'https://laravelseedergenerator.james-nock.co.uk/tools/laravelseedergenerator.php',
+        url: 'https://www.james-nock.co.uk/tools/laravelseedergenerator.php',
         data: formData,
         headers: { 'Content-Type': 'multipart/form-data' },
       })
